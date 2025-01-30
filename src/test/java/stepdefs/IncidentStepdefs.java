d22f4dd1ed83d707db8fe5ddb4f383ba4e0d759e
robertobertaccini <roberto.bertaccini@gmail.com>
2025-01-30 13:06:23 +0100

    Incident E2E

--- src/test/java/stepdefs/IncidentStepdefs.java
+++ src/test/java/stepdefs/IncidentStepdefs.java
@@ -16,7 +16,11 @@
 
 package stepdefs;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import com.google.inject.Inject;
@@ -26,6 +30,7 @@ import com.pega.prpc.workobjects.EditCaseType;
 import com.pega.test.pega_sample_testframework.PlatformBrowser;
 import com.pega.test.pega_sample_testframework.PlatformObjectsBean;
 import com.pega.test.pega_sample_testframework.PlatformTestEnvironment;
+import com.pega.util.XPathUtil;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -39,6 +44,36 @@ public class IncidentStepdefs {
 	private EditCaseType editCaseType;
 	private PlatformObjectsBean objBean;
 	
+	public static final By INCIDENT_TYPE =  byTestID("IncidentType:select:control");
+	public static final By INCIDENT_SUBTYPE = byTestID("IncidentSubType:select:control");
+	public static final By NEXT =  By.xpath("//button[@name='next']");
+	public static final By SEARCH_BY = byTestID("Search by::select:control");
+	public static final By COST = byTestID("Cost:currency-input:control");
+	public static final By FILL_FORM_WITH_AI =  By.xpath("//span[text()='Fill form with AI']/ancestor::button");
+	public static final By RESOLUTION_METHOD = byTestID("What would you like Sweet Life Inc  to do to resolve your issue?:select:control");
+	public static final By SUBMIT =  By.xpath("//button[@name='submit']");
+	
+	public static final By UserConsent =  By.xpath("//*[@data-testid='UserConsent']/following-sibling::label/div");
+	public static final By PrivacyPolicy =  By.xpath("//*[@data-testid='PrivacyPolicy']/following-sibling::label/div");
+	
+	public static final By PROD_SEARCH =  byTestID("search");
+	public static final By CATEGORY_SEARCH =  byTestID("FilterCategory:input:control");
+	
+	
+	private static By byTestID(String TestID) {
+		
+		return By.xpath("//*[@data-testid=\""+TestID+"\"]");
+		
+	}
+	
+	private void GoSleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	@Inject
 	public IncidentStepdefs(PlatformTestEnvironment testEnv, PlatformObjectsBean objBean) {
@@ -52,33 +87,61 @@ public class IncidentStepdefs {
 	
 	@When("I select incident type {string} with subtype {string}")
 	public void i_select_incident_type_with_subtype(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		new WebDriverWait(pegaDriver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(NEXT));
+		pegaDriver.findSelectBox(INCIDENT_TYPE).selectByValue(string);
+		GoSleep(2000);
+		pegaDriver.findSelectBox(INCIDENT_SUBTYPE).selectByValue(string2);
+		pegaDriver.findElement(NEXT).click();
+
 	}
+	
 	@Then("I search by {string} having category like {string} and cost {int}")
 	public void i_search_by_having_category_like_and_cost(String string, String string2, Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		new WebDriverWait(pegaDriver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(NEXT));
+		pegaDriver.findSelectBox(SEARCH_BY).selectByValue(string);
+		pegaDriver.findElement(CATEGORY_SEARCH).sendKeys(string2);
+		pegaDriver.findElement(COST).sendKeys(int1.toString());
+		
+		pegaDriver.findElement(PROD_SEARCH).click();
+
 	}
+	
 	@When("i select the {string} product")
 	public void i_select_the_product(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		
+		By SEL_PROD =  By.xpath("//input[@aria-label='"+string+"']/following-sibling::label/div");
+		new WebDriverWait(pegaDriver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(SEL_PROD));
+		
+		pegaDriver.findElement(SEL_PROD,false).click();
+		pegaDriver.findElement(NEXT).click();
 	}
+	
 	@When("i provide contact info with manual address")
 	public void i_provide_contact_info_with_manual_address() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		new WebDriverWait(pegaDriver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(NEXT));
+		pegaDriver.findElement(FILL_FORM_WITH_AI).click();
+		GoSleep(5000);
+	   	pegaDriver.findElement(NEXT).click();
+		
 	}
+	
 	@When("i select {string} for resolution method")
 	public void i_select_for_resolution_method(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		new WebDriverWait(pegaDriver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(NEXT));
+		pegaDriver.findSelectBox(RESOLUTION_METHOD).selectByValue(string);
+		pegaDriver.findElement(NEXT).click();
 	}
+	
 	@Then("i review the case and submit")
 	public void i_review_the_case_and_submit() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		new WebDriverWait(pegaDriver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(SUBMIT));
+		pegaDriver.findElement(UserConsent).click();
+		pegaDriver.findElement(PrivacyPolicy).click();
+		pegaDriver.findElement(SUBMIT).click();
 	}
 	
 	
--- src/test/resources/features/Incident.feature
+++ src/test/resources/features/Incident.feature
@@ -5,8 +5,8 @@ Feature: Incident
     Given A User logs in as customer
     When I create a new incident
     And I select incident type "Product faulty or unsafe" with subtype "Product not as described"
-    Then I search by "Category" having category like "a" and cost 100
-    When i select the "Santa bell" product
+    Then I search by "Category" having category like "Chocollate" and cost 100
+    When i select the "Chocollate Pirates" product
     And i provide contact info with manual address
     And i select "Refund" for resolution method 
     Then i review the case and submit
